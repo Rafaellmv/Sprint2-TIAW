3ed5c485b502ba60e248f1cef5e03984f6e570e5
Rafael <105494527+Rafaellmv@users.noreply.github.com>
2022-10-22 13:23:19 -0300

    Update UserController.js

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -1,5 +1,20 @@
 class UserController{
   constructor(){
-      console.log('UserController foi instanciada.')
+      this.addEventBtns();
+  }
+  
+  
+  addEventBtns(){
+    document.querySelector('.add').addEventListener('click',()=>{
+        document.querySelector('.form-add').style.display = 'flex'
+    })
+    
+    document.querySelectorAll('.close')[0].addEventListener('click',()=>{
+      document.querySelector('.form-add').style.display = 'none'
+  })
+    
+    document.querySelectorAll('.close')[1].addEventListener('click',()=>{
+      document.querySelector('.form-edit').style.display = 'none'
+})
   }
 }
